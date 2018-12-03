f68c693809f170eeea9e1a0b699e5a571bdfbdf4
Matteo Corti <matteo@corti.li>
2018-12-03 09:09:45 +0100

    Initial import

---
+++ bakup.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# This script does personal backups to a rsync backup server. You will end up
+# with a 7 day rotating incremental backup. The incrementals will go
+# into subdirectories named after the day of the week, and the current
+# full backup goes into a directory called "current"
+# tridge@linuxcare.com
+
+# directories to backup
+DIR="/root \
+/etc \
+/home \
+/var/lib/imap \
+/var/lib/mailman \
+/var/lib/mysql \
+/var/log \
+/var/spool/imap \
+/var/www"
+
+EXCLUDES="--exclude=/var/lib/imap/socket/lmtp \
+--exclude=/var/lib/mysql/mysql.sock \
+--exclude=/var/lib/imap/proc \
+--exclude=/var/log \
+--exclude=/home/nobody/ \
+--exclude=/var/spool/imap/*/user/*/Drafts/* \
+--exclude=/var/spool/imap/**.NEW"
+
+# the name of the backup machine
+BSERVER=casa.corti.li
+BUSER=corti
+BDIR=/Volumes/External/corti.li
+
+# Emergency
+#BSERVER=matteo.ethz.ch
+#BUSER=corti
+#BDIR=/home/corti/corti.li
+
+
+########################################################################
+
+OPTS="--force \
+--ignore-errors \
+--delete \
+$EXCLUDES \
+--archive \
+--compress \
+--partial \
+--relative"
+
+# verbose
+# OPTS="$OPTS -v"
+
+rsync --rsh 'ssh -x ' $OPTS $DIR $BUSER@$BSERVER:$BDIR
